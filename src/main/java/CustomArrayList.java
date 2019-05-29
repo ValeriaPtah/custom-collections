e60dba91f4f0c486d4c200b7cdd166e190a7e8fa
ValeriePtah <it2ava@gmail.com>
2019-05-29 22:58:38 +0300

    cleaned up JavaDoc, added SIZE as a field, added rescale() for adjustments

--- src/main/java/CustomArrayList.java
+++ src/main/java/CustomArrayList.java
@@ -1,20 +1,10 @@
-/**
- * Make smth ArrayList like
- * First of all you expect the following methods:
- * `public int size()` -> return size of the list
- * and the implementation detail you need to know about
- * you store everything internally in an array, say `T[]` of size 10
- * when you see your array gets full (say capacity 80%, or in this case, has 8 elements) you move all elements over to a new array of size 2*oldSize
- * so in this case 20, then 40, then 80…
- * if you remove and you see you only use 25% of capacity, then move elements over in an array half the size
- *
- * @param <T>
- */
+import java.util.Arrays;
 
 public class CustomArrayList<T> implements CustomList<T> {
 
   private static int INIT_CAPACITY = 10;
   private Object[] array;
+  private int size;
 
   public CustomArrayList() {
     array = new Object[INIT_CAPACITY];
@@ -30,10 +20,10 @@ public class CustomArrayList<T> implements CustomList<T> {
   /**
    * Adds element of type T to the end of the list
    *
-   * @param el
+   * @param el that is added
    */
   public void add(T el) {
-
+    size++;
   }
 
   /**
@@ -41,9 +31,9 @@ public class CustomArrayList<T> implements CustomList<T> {
    * All element after index shift one (e.g. `[1, 2, 3]` is your list, if you do `.add(5, 1)` it becomes `[1, 5, 2, 3]`.
    * Throws `ArrayIndexOutOfBoundsException` if the element index is outside of range
    *
-   * @param el
-   * @param index
-   * @throws ArrayIndexOutOfBoundsException
+   * @param el    that is added
+   * @param index where an element should be added
+   * @throws ArrayIndexOutOfBoundsException if the element index is outside of range
    */
   public void add(T el, int index) throws ArrayIndexOutOfBoundsException {
 
@@ -51,18 +41,34 @@ public class CustomArrayList<T> implements CustomList<T> {
 
   /**
    * Removes an element at index i, shifting all back, e.g. `[1, 2, 3]` after removal at 1 list to becomes `[2, 3]`
+   * If after removal only 25% of capacity is used, then moves elements over in an array half the size
    *
-   * @param index
+   * @param index of an element that should be removed
    * @return removed element
-   * @throws ArrayIndexOutOfBoundsException
+   * @throws ArrayIndexOutOfBoundsException if the element index is outside of range
    */
   @SuppressWarnings("unchecked")
   public T remove(int index) throws ArrayIndexOutOfBoundsException {
-    Object removed = this.array[index];
+    if (index < 0 || index >= size) {
+      throw new ArrayIndexOutOfBoundsException("Incorrect index, nothing at this position");
+    }
+    Object removed = array[index];
+    System.arraycopy(array, index + 1, array, index, size - index - 1);
+    array[--size] = null;
+    if (size < array.length / 4) {
+      this.rescale(array.length / 2);
+    }
     return (T) removed;
   }
 
+  /**
+   * @return size of the array (amount of elements it holds
+   */
   public int size() {
-    return array.length;
+    return size;
+  }
+
+  private void rescale(int newCapacity) {
+    array = Arrays.copyOf(array, newCapacity);
   }
 }
---
+++ src/test/java/CustomArrayListTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomArrayListTest {
+
+  @Test
+  void add() {
+
+  }
+
+  @Test
+  void add1() {
+  }
+
+  @Test
+  void remove() {
+  }
+
+  @Test
+  void size() {
+  }
+}
